94ea69230824679853bcf39e3df3b8fafa9d10ca
Ethan Kent <ekent@jd11.law.harvard.edu>
2025-01-05 11:33:34 -0500

    still working with build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/holding-quiz/",
+  base: "/holding-quiz/", // Match your repository name for GitHub Pages
+  build: {
+    outDir: "dist", // Default build directory
+  },
   plugins: [react()],
 });
