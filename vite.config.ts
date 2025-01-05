5f3aab07b806065c2add9ef720b2bd4b44e7d7d9
Ethan Kent <ekent@jd11.law.harvard.edu>
2025-01-05 11:20:12 -0500

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/holding-quiz/",
   plugins: [react()],
-  build: {
-    outDir: "dist", // Default is 'dist'. Change to 'build' if needed.
-  },
 });
